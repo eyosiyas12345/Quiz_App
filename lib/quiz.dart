e48011d712799b482d32a8d1c59179ea1db4a1c9
Eyosiyas_Gezahegn <eyosiyasgezahegn326@gmail.com>
2025-11-24 12:47:36 +0000

    new file created

---
+++ lib/quiz.dart
@@ -0,0 +1,20 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Quize extends StatefulWidget {
+  Quize({super.key});
+  
+  @override
+  State createState (){
+    return 
+  }
+}
+class _QuizeState extends State<Quize>{
+  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SplashScreen(),
+      ),
+    );
+}
---
+++ lib/quiz_questions.dart
@@ -0,0 +1,7 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Quize_Questions extends StatefulWidget {
+  Quize_Questions({super.key});
+}
