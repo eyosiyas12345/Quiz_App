0bf4bb840e1f5af59753092e91be4e82428f1a05
Eyosiyas_Gezahegn <eyosiyasgezahegn326@gmail.com>
2025-11-24 17:33:19 +0000

    modified parent file for navigation

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,20 +1,48 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:quize_app/splash_screen.dart';
+import 'package:quize_app/quiz_questions.dart';
 
-class Quize extends StatefulWidget {
-  Quize({super.key});
-  
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
   @override
-  State createState (){
-    return 
+  State<Quiz> createState() {
+    return _QuizState();
   }
 }
-class _QuizeState extends State<Quize>{
-  MaterialApp(
+
+class _QuizState extends State<Quiz> {
+  late Widget activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = SplashScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuizQuestions();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: SplashScreen(),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Color(0xff460252), Color(0xffa31eba)],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+          ),
+          child: activeScreen,
+        ),
       ),
     );
+  }
 }
