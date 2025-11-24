0fd045a7f16cd41c7d76035adf48fc3ab928e5c5
Eyosiyas_Gezahegn <eyosiyasgezahegn326@gmail.com>
2025-11-24 17:33:51 +0000

    sample text added

--- lib/quiz_questions.dart
+++ lib/quiz_questions.dart
@@ -1,7 +1,24 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
-class Quize_Questions extends StatefulWidget {
-  Quize_Questions({super.key});
+class QuizQuestions extends StatefulWidget {
+  const QuizQuestions({super.key});
+  @override
+  State<QuizQuestions> createState() {
+    return _QuizQuestionsState();
+  }
+}
+
+class _QuizQuestionsState extends State<QuizQuestions> {
+  @override
+  Widget build(context) {
+    return Center(
+      child: const Text(
+        "Hello Questions.",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 24,
+        ),
+      ),
+    );
+  }
 }
