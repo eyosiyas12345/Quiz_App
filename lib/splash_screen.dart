bfce4c4e126f917a34aea8293f15f07eae08dbc4
Eyosiyas_Gezahegn <eyosiyasgezahegn326@gmail.com>
2025-11-24 12:47:48 +0000

    modified

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -4,13 +4,60 @@ class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
   @override
   Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            colors: [Color(0xff460252), Color(0xffa31eba)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight),
+      ),
+      child: const Contents(),
+    );
+  }
+}
+
+class Contents extends StatelessWidget {
+  const Contents({super.key});
+
+  @override
+  Widget build(context) {
     return Center(
-        child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Image.asset('assets/images/quiz-logo.png', width: 300),
-        const Text('Learn Flutter the fun way!'),
-      ],
-    ));
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: Color(0xaaffffff),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {
+              print("Hello quize");
+            },
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              textStyle: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            icon: const Icon(Icons.arrow_right_alt, color: Colors.white),
+            label: const Text("Start"),
+          ),
+        ],
+      ),
+    );
   }
 }
