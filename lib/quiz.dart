b8a12ae7a7c8f075d5e2d685447720c4d1f97f5a
Eyosiyas_Gezahegn <eyosiyasgezahegn326@gmail.com>
2025-12-09 18:53:16 +0000

    simplified conditional rendering using ternary operator

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,10 +1,60 @@
+// import 'package:flutter/material.dart';
+// import 'package:quize_app/splash_screen.dart';
+// import 'package:quize_app/quiz_questions.dart';
+
+// class Quiz extends StatefulWidget {
+//   const Quiz({super.key});
+
+//   @override
+//   State<Quiz> createState() {
+//     return _QuizState();
+//   }
+// }
+
+// class _QuizState extends State<Quiz> {
+//   Widget? activeScreen;
+
+//   @override
+//   void initState() {
+//     activeScreen = SplashScreen(switchScreen);
+//     super.initState();
+//   }
+
+//   void switchScreen() {
+//     setState(() {
+//       activeScreen = const QuizQuestions();
+//     });
+//   }
+
+//   @override
+//   Widget build(context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Container(
+//           width: double.infinity,
+//           height: double.infinity,
+//           decoration: const BoxDecoration(
+//             gradient: LinearGradient(
+//                 colors: [Color(0xff460252), Color(0xffa31eba)],
+//                 begin: Alignment.topLeft,
+//                 end: Alignment.bottomRight),
+//           ),
+//           child: activeScreen,
+//         ),
+//       ),
+//     );
+//   }
+// }
+import 'dart:html';
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:quize_app/questions_screen.dart';
 import 'package:quize_app/splash_screen.dart';
-import 'package:quize_app/quiz_questions.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
-
   @override
   State<Quiz> createState() {
     return _QuizState();
@@ -12,17 +62,11 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
+  var activeScreen = 'splash_screen';
 
-  @override
-  void initState() {
-    activeScreen = SplashScreen(switchScreen);
-    super.initState();
-  }
-
-  void switchScreen() {
+  switchScreen() {
     setState(() {
-      activeScreen = const QuizQuestions();
+      activeScreen = 'questions_screen';
     });
   }
 
@@ -36,11 +80,14 @@ class _QuizState extends State<Quiz> {
           height: double.infinity,
           decoration: const BoxDecoration(
             gradient: LinearGradient(
-                colors: [Color(0xff460252), Color(0xffa31eba)],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight),
+              colors: [Color(0xff460252), Color(0xffa31eba)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
-          child: activeScreen,
+          child: activeScreen == 'splash_screen'
+              ? SplashScreen(switchScreen)
+              : const QuestionsScreen(),
         ),
       ),
     );
